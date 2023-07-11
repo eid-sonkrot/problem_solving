28992f8d39b0b7e0317f35532cbcd3b27baf6c24
fdfd <eid.com>
2023-07-11 20:28:35 +0300

    Refactor code for improved readability and maintainability

--- main.cpp
+++ main.cpp
@@ -1,34 +1,32 @@
-/******************************************************************************
+#include <iostream>
+#include <vector>
 
-Welcome to GDB Online.
-GDB online is an online compiler and debugger tool for C, C++, Python, Java, PHP, Ruby, Perl,
-C#, OCaml, VB, Swift, Pascal, Fortran, Haskell, Objective-C, Assembly, HTML, CSS, JS, SQLite, Prolog.
-Code, Compile, Run and Debug online from anywhere in world.
-
-*******************************************************************************/
-#include <bits/stdc++.h>
-#define int long long 
 using namespace std;
-vector<int>pow10(18);
-int numOfDigiht(int n,int i)
-{
-  
-  if(n>pow10[i])
-  {
-      return pow10[i]*(i)+numOfDigiht(n-pow10[i],i+1);
-  }else return (n+1)*(i);
+
+vector<long long> pow10(18);
+
+// Function to calculate the summation of the number of digits for every number from 0 to n
+int numberOfDigits(long long number, int power) {
+    if (number <= pow10[power])
+        return (number + 1) * power; // Sum of digits for numbers up to the given power
+    else
+        return pow10[power] * power + numberOfDigits(number - pow10[power], power + 1); // Recursive calculation for remaining digits
 }
-void solution()
-{
- int l,r;
-cin>>l>>r;
-pow10[0]=1;
-for(int i=1;i<18;i++)
-pow10[i]=pow10[i-1]*10ll;
-cout<<numOfDigiht(max(l,r),1)-numOfDigiht(min(l,r)-1,1)<<endl;   
+
+void solution() {
+    long long l, r;
+    cin >> l >> r;
+
+    pow10[0] = 1;
+    for (int i = 1; i < 18; i++) {
+        pow10[i] = pow10[i - 1] * 10LL; // Generate powers of 10 using the previous power
+    }
+
+    // Calculate the summation of the number of digits for numbers between l and r
+    cout << numberOfDigits(max(l, r), 1) - numberOfDigits(min(l, r) - 1, 1) << endl;
 }
-int32_t main()
-{
-solution();
+
+int main() {
+    solution();
     return 0;
 }
